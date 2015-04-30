a7dd4ba6eaa521d016d4b436996ccbc4775836d9
f7k <c083f57c786@hotmail.com>
2015-04-30 22:50:53 +0200

    Update simplecov & rspec params

--- rails_root/spec/rails_helper.rb
+++ rails_root/spec/rails_helper.rb
@@ -5,13 +5,14 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rspec/rails"
 require "simplecov"
 
-SimpleCov.start if ENV["COVERAGE"]
+SimpleCov.start("rails") if ENV["COVERAGE"]
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.disable_monkey_patching!
   config.infer_spec_type_from_file_location!
   config.order = :random
 end
